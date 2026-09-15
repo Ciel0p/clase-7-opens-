b5a89225634fd83bbcf0f5e9c484df891080a366
Cielo <u202424216@upc.edu.pe>
2026-09-15 11:26:35 -0500

    feat(i18n):add ngx-translate for internationalization support with English and Spanish translations

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,16 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { provideTranslateService } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideHttpClient()],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideHttpClient(),
+    provideTranslateService({
+      loader: provideTranslateHttpLoader({ prefix: './i18n/', suffix: '.json' }),
+      fallbackLang: 'en',
+    }),
+  ],
 };
